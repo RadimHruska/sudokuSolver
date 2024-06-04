eabb89fb375d9a27f502543ba9b4df6894e2c118
RadimHruska <hruska@bbnsro.info>
2024-06-04 21:03:46 +0200

    is safe function

--- sudokuSolever/sudokuSolever/solveSudoku.cs
+++ sudokuSolever/sudokuSolever/solveSudoku.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,30 +12,75 @@ namespace sudokuSolever
 {
     internal class solveSudoku
     {
-        public NumericUpDown[,] SolveSudoku(NumericUpDown[,] numericUpDowns)
+        private int[,] Matrix = new int[9,9];
+        private bool isPosible (int num, int row, int column)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (Matrix[row, i] == num)
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (Matrix[i, column] == num)
+                {
+                    return false;
+                }
+            }
+
+            int startRow = row - row % 3;
+            int startColumn = column - column % 3;
+
+            for (int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startColumn; j < startColumn + 3; j++)
+                {
+                    if (Matrix[i,j] == num)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool SolveSudoku()
+        {
+
+            return true;
+        }
+        public NumericUpDown[,] SolvedSudoku(NumericUpDown[,] numericUpDowns)
            {
-            int[,] matrix = new int[9, 9];
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
                     var numeric = numericUpDowns[i, j] as NumericUpDown;
-                    matrix[i,j] = (int)numeric.Value;
+                    Matrix[i,j] = (int)numeric.Value;
                 }
             }
 
-
-
-            NumericUpDown[,] listOfSolvedNumerics = new NumericUpDown[9, 9];
-            for (int i = 0; i < 9; i++)
+            if (SolveSudoku())
             {
-                for (int j = 0; j < 9; j++)
+                NumericUpDown[,] listOfSolvedNumerics = new NumericUpDown[9, 9];
+                for (int i = 0; i < 9; i++)
                 {
-                    var numeric = listOfSolvedNumerics[i, j] as NumericUpDown;
-                    numeric.Value = matrix[i,j];
+                    for (int j = 0; j < 9; j++)
+                    {
+                        var numeric = listOfSolvedNumerics[i, j] as NumericUpDown;
+                        numeric.Value = Matrix[i,j];
+                    }
                 }
+                return listOfSolvedNumerics;
             }
-            return listOfSolvedNumerics;
+            else
+            {
+                return numericUpDowns;
+            }
+
+            
            }
     }
 }
