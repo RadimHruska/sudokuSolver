2a9734cbd9310ca487d61c8a8769073aa9b9ef10
RadimHruska <hruska@bbnsro.info>
2024-06-04 18:49:48 +0200

    transfer to int and back

---
+++ sudokuSolever/sudokuSolever/solveSudoku.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudokuSolever
+{
+    internal class solveSudoku
+    {
+        public NumericUpDown[,] SolveSudoku(NumericUpDown[,] numericUpDowns)
+           {
+            int[,] matrix = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var numeric = numericUpDowns[i, j] as NumericUpDown;
+                    matrix[i,j] = (int)numeric.Value;
+                }
+            }
+
+
+
+            NumericUpDown[,] listOfSolvedNumerics = new NumericUpDown[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var numeric = listOfSolvedNumerics[i, j] as NumericUpDown;
+                    numeric.Value = matrix[i,j];
+                }
+            }
+            return listOfSolvedNumerics;
+           }
+    }
+}
