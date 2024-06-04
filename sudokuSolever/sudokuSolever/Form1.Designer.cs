f1d236e95698aca0fc4aee6340af37096c5143d4
RadimHruska <hruska@bbnsro.info>
2024-06-05 00:14:23 +0200

    save and load

--- sudokuSolever/sudokuSolever/Form1.Designer.cs
+++ sudokuSolever/sudokuSolever/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,14 +44,36 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Solve);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(444, 23);
+            this.button2.TabIndex = 82;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Save);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(444, 23);
+            this.button3.TabIndex = 83;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Load);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 425);
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.MaximumSize = new System.Drawing.Size(498, 464);
-            this.MinimumSize = new System.Drawing.Size(498, 464);
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
             this.Name = "Form1";
             this.Text = "Sudoku Solver";
             this.ResumeLayout(false);
@@ -58,6 +82,8 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- sudokuSolever/sudokuSolever/Form1.cs
+++ sudokuSolever/sudokuSolever/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,6 +60,63 @@ namespace sudokuSolever
         private void Solve(object sender, EventArgs e)
         {
             listOfNumerics = SudokuSolver.SolvedSudoku(listOfNumerics);
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            int[] numbers = new int[81];
+            int index = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    numbers[index++] = (int)listOfNumerics[i,j].Value;
+                }
+            }
+            string data = string.Join(";", numbers);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Sudoku File|*.sf";
+            saveFileDialog.Title = "Uložit sudoku";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    writer.Write(data);
+                }
+            }
+
+               
+           
+        }
+
+        private void Load(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Sudoku File|*.sf";
+            openFileDialog.Title = "Otevřít sudoku soubor";
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                {
+                    string data = reader.ReadToEnd();
+                    string[] stringNumbers = data.Split(';');
+                    int[] numbers = Array.ConvertAll(stringNumbers, int.Parse);
+                    int row = 0;
+                    int collumn = 0;
+                    foreach (var item in numbers)
+                    {
+                        listOfNumerics[row, collumn].Value = item;
+                        collumn++;
+                        if (collumn == 9)
+                        {
+                            collumn = 0;
+                            row++;
+                        }
+                    }
+                }
+            }
         }
     }
 }
