6bee83528d2dfe0fbcf8a04c80c44d267fe39cc3
RadimHruska <hruska@bbnsro.info>
2024-06-04 18:06:38 +0200

    generating numericUpDown grid

--- sudokuSolever/sudokuSolever/Form1.cs
+++ sudokuSolever/sudokuSolever/Form1.cs
@@ -12,9 +12,30 @@ namespace sudokuSolever
 {
     public partial class Form1 : Form
     {
+        public NumericUpDown [,] listOfNumerics = new NumericUpDown[9,9];
         public Form1()
         {
             InitializeComponent();
+            generateGrid();
+        }
+
+        private void generateGrid()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var numeric = new NumericUpDown();
+                    numeric.Value = 0;
+                    numeric.Maximum = 9;
+                    numeric.Minimum = 0;
+                    numeric.Width = 50;
+                    numeric.Location = new Point((numeric.Width+5) * i, (numeric.Height +5) * j);
+                    listOfNumerics[i, j] = numeric;
+                    Controls.Add(listOfNumerics[i, j]);
+                }
+            }
+
         }
     }
 }
